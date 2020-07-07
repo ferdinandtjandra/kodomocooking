8c867717c06aaad5629ca1b23d4cadc3bb090d55
ferd <you@example.com>
2020-07-07 15:50:48 +0700

    revision sendEail.php

--- sendEmail.php
+++ sendEmail.php
@@ -1,7 +1,5 @@
 <?php  
-
-// $base_url = "https://" . $_SERVER["HTTP_HOST"];
-$base_url = "http://" . $_SERVER["HTTP_HOST"] ."/kodomocooking";
+$base_url = "https://" . $_SERVER["HTTP_HOST"] ."/kodomocooking";
 
  
 //if user access except LP , redirect to top 
@@ -45,31 +43,30 @@ if (!empty($_POST['email'])) {
 // validation ok , so session destory
 session_destroy();
 
-
-
 mb_language("Japanese");
 mb_internal_encoding("UTF-8");
 $to = 'f.yuya.yamamoto@gmail.com';
 $subject = '料理教室にお問い合わせがありました。';
 $subject = mb_convert_encoding($subject, "ISO-2022-JP", "UTF-8,EUC-JP,auto");
-$message = "氏名（漢字）：
-            {$_POST['name']}
-            電話番号：
-            {$_POST['tel']}
-            メールアドレス：
-            {$_POST['email']}
-            住所：
-            {$_POST['address']}
-            お問い合わせ内容：
-            {$_POST['note']}";
+$message = <<<CONTENT
 
- 
+氏名（漢字）：
+{$_POST['name']}
+電話番号：
+{$_POST['tel']}
+メールアドレス：
+{$_POST['email']}
+住所：
+{$_POST['address']}
+お問い合わせ内容：
+{$_POST['note']}
+CONTENT;
 
 $message = mb_convert_encoding($message, "ISO-2022-JP", "UTF-8,EUC-JP,auto");
 
 $headers = "From: " .mb_encode_mimeheader("ease") ."<f.yuya.yamamoto@gmail.com>";
 
-$send_mail =  mail($to, $subject, $message, $headers);
+$send_mail =  mb_send_mail($to, $subject, $message, $headers);
 
 if($send_mail) {
     // header("Location: {$base_url}/thanks.php");  
@@ -79,5 +76,5 @@ if($send_mail) {
     header("Location: {$base_url}/?e=1#formease"); 
     exit;
 }
-
+ 
 ?>
